dcbf2428687896242477d25995394dc65858f345
areyomad <158549771+areyomad@users.noreply.github.com>
2025-05-25 08:05:32 +0200

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE uzytkownicy;
-
-USE uzytkownicy;
-
-CREATE TABLE users (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       login VARCHAR(255) NOT NULL UNIQUE,
-                       haslo VARCHAR(255) NOT NULL
-);
